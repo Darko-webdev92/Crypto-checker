8e99a804ea2d6c2f4ee23b43edb276ecb09d6670
Darko-webdev92 <37109758+Darko-webdev92@users.noreply.github.com>
2022-12-25 20:25:54 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CurrencyService } from './service/currency.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'crypto-checker';
+  selectedCurrency : string = "USD";
+
+  constructor(private currencyService: CurrencyService, private router: Router,){
+
+  }
+
+  sendCurrency(event:string){
+    console.log(event);
+    this.currencyService.setCurrency(event);
+  }
+
+  goToHome(){
+    this.router.navigate(['coin-list'])
+  }
+}
